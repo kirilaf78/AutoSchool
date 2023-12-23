690e03ab86f7eae6af2128103166f8ba5090ef18
Kiryl Afanasyeu <Kiryl.Afanasyeu@redsailtechnologies.com>
2023-12-23 11:00:35 -0500

    Task 10.2

---
+++ UnitTest1.cs
@@ -0,0 +1,219 @@
+
+namespace TestProject3
+{
+    using NUnit.Framework;
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
+    using OpenQA.Selenium.Support.UI;
+    using System.Security.Cryptography.X509Certificates;
+
+    public class LoginPage
+    {
+        IWebDriver webDriver;
+
+        public LoginPage(IWebDriver webDriver)
+        {
+            this.webDriver = webDriver;
+        }
+
+        private IWebElement RegEmailInput => webDriver.FindElement(By.XPath("//input[@id='reg_email']"));
+        private IWebElement RegPasswordInput => webDriver.FindElement(By.XPath("//input[@id='reg_password']"));
+        private IWebElement RegisterButton => webDriver.FindElement(By.XPath("//input[@name='register']"));
+        private IWebElement LoginUsername => webDriver.FindElement(By.XPath("//input[@id='username']"));
+        private IWebElement LoginPassword => webDriver.FindElement(By.XPath("//input[@id='password']"));
+        private IWebElement LoginButton => webDriver.FindElement(By.XPath("//input[@name='login']"));
+
+        private IWebElement AccountExistsMessage => webDriver.FindElement(By.XPath("//li[contains(text(),'An account is already registered with your email a')]"));
+        private IWebElement SuccessMessage => webDriver.FindElement(By.XPath("//p[contains(text(),'Hello')]"));
+        private IWebElement NoRegPassword => webDriver.FindElement(By.XPath("//div[@id='body'][.//li[contains(., 'Error') and contains(., 'Please enter an account password.')]]"));
+        private IWebElement NoRegEmail => webDriver.FindElement(By.XPath("//div[@id='body'][.//li[contains(., 'Error') and contains(., 'Please provide a valid email address.')]]"));
+
+        private IWebElement LostYourPassword => webDriver.FindElement(By.XPath("//a[normalize-space()='Lost your password?']"));
+        private IWebElement RememberMe => webDriver.FindElement(By.XPath("//label[normalize-space()='Remember me']"));
+
+
+        public LoginPage EnterRegEmail(string email)
+        {
+            RegEmailInput.SendKeys(email);
+            return this;
+        }
+
+        public LoginPage EnterRegPassword(string password)
+        {
+            RegPasswordInput.SendKeys(password);
+            return this;
+        }
+
+        public LoginPage ClickRegisterButton()
+        {
+            RegisterButton.Click();
+            return this;
+        }
+        public LoginPage EnterUserName(string email)
+        {
+            LoginUsername.SendKeys(email);
+            return this;
+        }
+
+        public LoginPage EnterPassword(string password)
+        {
+            LoginPassword.SendKeys(password);
+            return this;
+        }
+
+        public LoginPage ClickLoginButton()
+        {
+            LoginButton.Click();
+            return this;
+        }
+
+        public bool IsAccountExistMessage()
+        {
+            WebDriverWait wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(5));
+            IWebElement element = wait.Until(e => AccountExistsMessage);
+            return AccountExistsMessage.Displayed;
+        }
+
+        public bool IsLoginSuccessfull()
+        {
+            WebDriverWait wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(3));
+            IWebElement element = wait.Until(e => SuccessMessage);
+            return SuccessMessage.Displayed;
+        }
+        public bool IsPasswordNeededToReg()
+        {
+            WebDriverWait wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(3));
+            IWebElement element = wait.Until(e => NoRegPassword);
+            return NoRegPassword.Displayed;
+
+        }
+
+        public bool NoRegEmailWarning()
+        {
+            WebDriverWait wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(3));
+            IWebElement element = wait.Until(e => NoRegEmail);
+            return NoRegEmail.Displayed;
+
+        }
+
+        public string GetLostYourPasswordText()
+        {
+            return LostYourPassword.Text;
+        }
+
+        public string GetRememberMeText()
+        {
+            return RememberMe.Text;
+        }
+
+        public string GetRegisterText()
+        {
+            return RegisterButton.GetAttribute("value");
+        }
+
+    }
+    public class LoginPageTest
+    {
+
+        IWebDriver webDriver;
+        LoginPage loginPage;
+        string URL = "https://practice.automationtesting.in/my-account/";
+
+        [SetUp]
+        public void SetUp()
+        {
+            webDriver = new ChromeDriver();
+            loginPage = new LoginPage(webDriver);
+            webDriver.Navigate().GoToUrl(URL);
+            webDriver.Manage().Window.Maximize();
+            try
+            {
+                IWebElement consentButton = webDriver.FindElement(By.XPath("//p[@class='fc-button-label' and text()='Consent']"));
+                consentButton.Click();
+            }
+            catch (NoSuchElementException)
+            {
+                Console.WriteLine("No such an element!");
+            }
+
+        }
+        // account is already registered message
+
+        [Test]
+        public void IsAccountRegistered()
+        {
+            loginPage.EnterRegEmail("karage16255@bayxs.com")
+                     .EnterRegPassword("222uSNj%g")
+                     .ClickRegisterButton();
+            Assert.That(loginPage.IsAccountExistMessage(), Is.EqualTo(true), "The account exists message is not displayed");
+        }
+
+        [Test]
+
+        public void IsLoginSuccess()
+        {
+            loginPage.EnterUserName("karage16255@bayxs.com")
+                     .EnterPassword("222uSNj%g")
+                     .ClickLoginButton();
+            Assert.That(loginPage.IsLoginSuccessfull(), Is.EqualTo(true), "The success message is not displayed");
+
+        }
+        // error message displayed when user did not enter password on registration
+
+        [Test]
+        public void IsNoRegPasswordWarning()
+        {
+            loginPage.EnterRegEmail("sarage16255@bayxs.com")
+                     .ClickRegisterButton();
+
+            Assert.That(loginPage.IsPasswordNeededToReg, Is.EqualTo(true), "The success message is not displayed");
+
+        }
+        // error message displayed when user did not enter email on registration
+
+
+        [Test]
+
+        public void IsNoRegEmailWarnign()
+        {
+            loginPage.EnterRegPassword("222uSNj%g")
+                     .ClickRegisterButton();
+
+            Assert.That(loginPage.NoRegEmailWarning, Is.EqualTo(true), "The success message is not displayed");
+
+        }
+
+        [Test]
+
+        public void IsLostPasswordText()
+        {
+            string expectedResult = "Lost your password?";
+            Assert.That(loginPage.GetLostYourPasswordText(), Is.EqualTo(expectedResult), "The text doesn't equal to expectedResult");
+        }
+
+
+        [Test]
+
+        public void IsRememberMeText() 
+        {
+            string expectedResult = "Remember me";
+            Assert.That(loginPage.GetRememberMeText(), Is.EqualTo(expectedResult), "The text doesn't equal to expectedResult");
+        }
+
+        [Test]
+
+        public void IsRegisterText() 
+        {
+            string expectedResult = "Register";
+            Assert.That(loginPage.GetRegisterText(), Is.EqualTo(expectedResult), "The text doesn't equal to expectedResult");
+        }
+
+        
+
+        [TearDown]
+        public void TearDown()
+        {
+            webDriver.Close();
+        }
+    }
+}  
