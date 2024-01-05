0939cc1f4a586c2d4ca17d64557ac8de1dc73215
Kiryl Afanasyeu <Kiryl.Afanasyeu@redsailtechnologies.com>
2024-01-05 04:44:37 -0500

    adding sale-price test and some minor changes

--- UnitTest2.cs
+++ UnitTest2.cs
@@ -4,18 +4,18 @@ namespace TestProject2
     using OpenQA.Selenium;
     using OpenQA.Selenium.Chrome;
     using OpenQA.Selenium.Support.UI;
-    using System.Drawing;
-    using System.Xml.Linq;
-    using static System.Net.Mime.MediaTypeNames;
 
     public class SelenuimWebDriverTest
     {
         IWebDriver webdriver;
-        
+
         // links with names of all products on the main page 
+        public string title = "HTML";
+        public string productName = "Thinking in HTML";
+        public string saleTitle = "SALE!";
+        public int quantity = 3;
+
         public string producstLinks(string productName) => $"//a[contains(@href, 'https') and descendant::h3[text()='{productName}']]";
-
-
 
 
         [SetUp]
@@ -23,6 +23,8 @@ namespace TestProject2
         {
             webdriver = new ChromeDriver();
             webdriver.Navigate().GoToUrl("https://practice.automationtesting.in/shop/");
+            webdriver.Manage().Window.Maximize();
+
 
             try
             {
@@ -40,22 +42,20 @@ namespace TestProject2
         public void IsTitleDisplayed()
         {
 
-
             IWebElement searchInput = webdriver.FindElement(By.XPath("//input[@id='s']"));
 
-            searchInput.SendKeys("HTML" + Keys.Enter);
+            searchInput.SendKeys(title + Keys.Enter);
 
-            string expectedTitle = "HTML";
-            IWebElement element = webdriver.FindElement(By.XPath("//em[contains(text(), 'HTML')]"));
-            string actualTitle = element.Text;
+            IWebElement pageTitle = webdriver.FindElement(By.XPath($"//h1[@class='page-title']"));
+            string actualTitle = pageTitle.Text;
 
-            Assert.That(expectedTitle, Is.EqualTo(actualTitle), $"The title: {actualTitle} must be equal to {expectedTitle}");
+            Assert.That(actualTitle, Does.Contain(title), $"The title: {actualTitle} must contain {title}");
         }
 
         [Test]
         public void AreProductNamesLinks()
         {
-            string[] productNames = 
+            string[] productNames =
             {
              "Android Quick Start Guide",
              "Functional Programming in JS",
@@ -79,6 +79,26 @@ namespace TestProject2
 
         }
 
+        [Test]
+
+        public void VerifySaleMarkPriceAndDiscount()
+        {
+            IWebElement productLink = webdriver.FindElement(By.XPath(producstLinks(productName)));
+            productLink.Click();
+            IWebElement saleMark = webdriver.FindElement(By.XPath("//span[@class='onsale']"));
+            IWebElement priceElement = webdriver.FindElement(By.XPath("//del//span[@class='woocommerce-Price-amount amount']"));
+            IWebElement discountPriceElement = webdriver.FindElement(By.XPath("//ins//span[@class='woocommerce-Price-amount amount']"));
+
+            string actualText = saleMark.Text;
+            string actualRegularPrice = priceElement.Text.Substring(1);
+            string actualDiscountedPrice = discountPriceElement.Text.Substring(1);
+
+            Assert.That(saleTitle, Is.EqualTo(actualText), $"The title: {actualText} must be equal to {saleMark}");
+            Assert.That(actualRegularPrice, Is.Not.Empty, "Regular price should be displayed");
+            Assert.That(actualDiscountedPrice, Is.Not.Empty, "Discounted price should be displayed");
+
+        }
+
 
         [Test]
         public void VerifyRelatedProduct()
@@ -87,21 +107,19 @@ namespace TestProject2
             IJavaScriptExecutor js = (IJavaScriptExecutor)webdriver;
             WebDriverWait wait = new WebDriverWait(webdriver, TimeSpan.FromSeconds(10));
 
-            int quantity = 3;
 
 
-            string productName = "Thinking in HTML";
             IWebElement productLink = webdriver.FindElement(By.XPath(producstLinks(productName)));
             productLink.Click();
 
             // opening related product
-            IWebElement relatedProduct = wait.Until(e=>e.FindElement(By.XPath("//img[contains(@title,\"HTML5 Web Application Development Beginner's guide\")]")));
+            IWebElement relatedProduct = wait.Until(e => e.FindElement(By.XPath("//img[@title=\"HTML5 Web Application Development Beginner's guide\"]")));
+
             js.ExecuteScript("arguments[0].scrollIntoView(true);", relatedProduct);
-            // знаю, что следующие 2 строчки вроде бы дублируют функционал, но так, почему-то тест более стабилен
+            // следующие 2 строчки вроде бы дублируют функционал, но так, почему-то тест более стабилен
             relatedProduct.Click();
             js.ExecuteScript("arguments[0].click();", relatedProduct);
-            
-            // Thread.Sleep(3000);
+
             // adding to basket 1 qty.
             IWebElement addToBasket = webdriver.FindElement(By.XPath("//button[normalize-space()='Add to basket']"));
             js.ExecuteScript("arguments[0].click();", addToBasket);
@@ -112,32 +130,33 @@ namespace TestProject2
             string relatedProductName = relatedProductElementName.Text;
 
             // saving related product price
-            IWebElement relatedProductElementPrice = webdriver.FindElement(By.XPath("//p[@class='price']//span[@class='woocommerce-Price-amount amount']"));
-            string relatedProductPrice = relatedProductElementPrice.Text;
-            string numericValue = relatedProductPrice.Substring(1);
+            IWebElement relatedProductPriceElement = webdriver.FindElement(By.XPath("//p[@class='price']//span[@class='woocommerce-Price-amount amount']"));
+            string relatedProductPrice = relatedProductPriceElement.Text.Substring(1);
 
             // calculating final product price 
-            double productPrice = Double.Parse(numericValue);
+            double productPrice = Double.Parse(relatedProductPrice);
             double priceForThreeItems = productPrice * quantity;
             string relatedProductFinalPrice = priceForThreeItems.ToString("F2");
 
-            // adding two more to make total of  3 in the basket
-            IWebElement productQuantity = webdriver.FindElement(By.XPath("//input[@title='Qty']"));
-            productQuantity.Clear();
-            productQuantity.SendKeys("2" + Keys.Enter);
+            // Setting the product quantity to a total of 3 in the basket
             IWebElement viewBasketLink = webdriver.FindElement(By.XPath("//a[@title='View your shopping cart']"));
             viewBasketLink.Click();
+            IWebElement productQuantityInBasket = webdriver.FindElement(By.XPath("//input[@title='Qty']"));
+            productQuantityInBasket.Clear();
+            productQuantityInBasket.SendKeys("3" + Keys.Enter);
 
+
+            Thread.Sleep(3000);
             // product name and price in the basket
-            IWebElement relatedProductElementNameInBasket = webdriver.FindElement(By.XPath("//a[normalize-space()='HTML5 WebApp Develpment']"));
+
+            IWebElement relatedProductElementNameInBasket = wait.Until(e => e.FindElement(By.XPath("//a[normalize-space()='HTML5 WebApp Develpment']")));
             string relatedProductNameInBasket = relatedProductElementNameInBasket.Text;
-            IWebElement relatedProductPriceElementInBasket = webdriver.FindElement(By.XPath("//td[@class='product-subtotal']//span[@class='woocommerce-Price-amount amount']"));
-            string relatedProductPriceInBasket = relatedProductPriceElementInBasket.Text;
-            string numericValueOfPriceInBasket = relatedProductPriceInBasket.Substring(1);
+            IWebElement relatedProductPriceElementInBasket = wait.Until(e => e.FindElement(By.XPath("//td[@class='product-subtotal']//span[@class='woocommerce-Price-amount amount']")));
+            string relatedProductPriceInBasket = relatedProductPriceElementInBasket.Text.Substring(1);
+            Console.WriteLine($"Actual Product Price in Basket: {relatedProductPriceInBasket}");
 
             Assert.That(relatedProductName, Is.EqualTo(relatedProductNameInBasket), "Product name is not the same as product name in the basket");
-            Assert.That(relatedProductFinalPrice, Is.EqualTo(numericValueOfPriceInBasket), "Product price times 3 is not equal to product price in the basket");
-
+            Assert.That(relatedProductFinalPrice, Is.EqualTo(relatedProductPriceInBasket), "Product price times 3 is not equal to product price in the basket");
         }
 
 
